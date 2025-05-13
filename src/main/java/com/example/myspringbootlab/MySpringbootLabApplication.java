efce491ebe53cf4348b03b5b5009b82238d41bd7
lamin5 <say8913@gmail.com>
2025-05-14 01:28:52 +0900

    first

---
+++ src/main/java/com/example/myspringbootlab/MySpringbootLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringbootlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootLabApplication.class, args);
+	}
+
+}
